183aa451b079afff38057ed01eeb48823e5bb7e1
zgrblghetto <118743174+zgrblghetto@users.noreply.github.com>
2022-11-22 14:20:34 +0200

    Add files via upload

---
+++ лабка посіпака.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(0, 0, 0, 0);
+
+        }
+
+        int n = 0;
+         private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 6);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 6, pictureBox1.Location.Y);
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 6);
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 6, pictureBox1.Location.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Screenshot_24;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(311, 119);
+        }
+    }
+}
